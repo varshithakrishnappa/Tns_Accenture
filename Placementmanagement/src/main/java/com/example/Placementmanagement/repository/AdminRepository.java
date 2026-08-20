27061466ac3616673e9971e26c1b1621d10a5862
Varshitha K <168709417+varshithakrishnappa@users.noreply.github.com>
2026-08-20 19:19:10 +0530

    Delete Placementmanagement directory

--- Placementmanagement/src/main/java/com/example/Placementmanagement/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Placementmanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Placementmanagement.entity.Adminentity;
-
-public interface AdminRepository extends JpaRepository<Adminentity, Integer> {
-
-}
